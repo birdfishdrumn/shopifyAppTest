b70807b0b4dba998c818cee646e8ae1ce86c1568
michihiro <pomeofishbird0303@gmail.com>
2021-09-11 08:59:43 +0900

    installページ,clientRouterページの設定

---
+++ pages/install.tsx
@@ -0,0 +1,12 @@
+import { Page } from '@shopify/polaris'
+import React from 'react'
+
+const install = () => {
+  return (
+    <Page>
+       Install baby
+    </Page>
+  )
+}
+
+export default install
