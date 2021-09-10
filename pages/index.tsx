9f74f1dfda601021b3186dacfda9d284674b20f0
michihiro <pomeofishbird0303@gmail.com>
2021-09-11 07:29:50 +0900

    settinStore.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,27 @@
 import { ResourcePicker} from "@shopify/app-bridge-react";
 import { SelectPayload } from "@shopify/app-bridge-react/components/ResourcePicker/ResourcePicker";
-
-import { EmptyState, Heading, Page } from "@shopify/polaris";
 import {useState,useEffect} from "react"
 import ProductEmptyState from "../components/ProductEmptyState";
 import ProductList from "../components/ProductList";
 import ProductPage from "../components/ProductPage";
+import store from "store-js";
 
-const Index = () => {
+const Index = ({host}) => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [products, setProducts] = useState([])
   const [productsId, setProductsId] = useState([])
 
   // 新しい商品が選択された時も最初にチェックボタンをつけておく機能
   useEffect(() => {
+    const productList = store.get(`${host}-products`)
+    if (productList) {
+          setProducts(productList)
+    }
+
+  }, []);
+
+  useEffect(() => {
+    console.log(host)
     const ids = products.map(product => {
       return {
         id: product.id
@@ -26,6 +34,7 @@ const Index = () => {
     setIsOpen(false);
 
     setProducts(payload.selection);
+    store.set(`${host}-products`,payload.selection)
   }
   return (
     <>
