76a8e0ca509cfc03801e2eacdd7a5ccedbbcc885
michihiro <pomeofishbird0303@gmail.com>
2021-09-11 14:45:13 +0900

    installページに 主要機能を実装

--- pages/install.tsx
+++ pages/install.tsx
@@ -1,10 +1,33 @@
-import { Page } from '@shopify/polaris'
-import React from 'react'
+import { Layout, Page, SettingToggle, TextStyle } from '@shopify/polaris'
+import React, { useState }from 'react'
 
 const install = () => {
+  const [isInstalled, setIsInstalled] = useState(null)
+  const titleDescription = isInstalled ? "Uninstall" : "Install"
+  const bodyDescription = isInstalled ?  "installed" : "uninstalled"
+  function handleAction() {
+     setIsInstalled(prevState => !prevState)
+  }
   return (
     <Page>
-       Install baby
+      <Layout.AnnotatedSection
+        title={`${titleDescription} banner`}
+        description="Toggle banner installation on your shop"
+      >
+        <SettingToggle
+          action={{
+            content: titleDescription,
+            onAction: handleAction
+
+          }}
+          enabled={true}
+        >
+          The banner script is <TextStyle variation="strong">
+            {bodyDescription}
+          </TextStyle>
+        </SettingToggle>
+      </Layout.AnnotatedSection>
+
     </Page>
   )
 }
