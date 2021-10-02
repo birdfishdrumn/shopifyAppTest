201ecc192dd6ad1d0e8ed9212c9dc4f02fd8ff59
michihiro <pomeofishbird0303@gmail.com>
2021-10-03 08:57:43 +0900

    themeファイルの更新機能の基本的な機能の実装

--- server/updateTheme/updateTheme.js
+++ server/updateTheme/updateTheme.js
@@ -1,7 +1,9 @@
 import Axios from "axios";
+import fs from "fs";
+import path from "path";
 const themeApi = "/admin/api/2021-07/";
 
-export function updateTheme(shop, accessToken) {
+export async function updateTheme(shop, accessToken) {
   const axios = Axios.create({
     baseURL: `https://${shop}/${themeApi}`,
     headers: {
@@ -9,7 +11,54 @@ export function updateTheme(shop, accessToken) {
       "X-Shopify-Access-Token": accessToken,
     },
   });
-  getThemeId(axios);
+  const mainThemeId = await getThemeId(axios);
+  if (!mainThemeId) {
+    return;
+  }
+  const newPage = await getAssetThemeLiquid(mainThemeId, axios);
+  // pageの名前は実際のliquidのディレクトリ構造を参照する。
+  const result = await uploadAssetTheme(
+    axios,
+    mainThemeId,
+    newPage,
+    "layout/theme.liquid"
+  );
+}
+// 既存のliquidに新しい行を追加する処理
+async function uploadAssetTheme(axios, id, page, pageName) {
+  const body = {
+    asset: {
+      key: pageName,
+      value: page,
+    },
+  };
+  const result = await axios.put(`/themes/${id}/assets.json`, body);
+  console.log("upload page", result);
+}
+
+async function getAssetThemeLiquid(id, axios) {
+  const { data } = await axios.get(
+    `/themes/${id}/assets.json?asset[key]=layout/theme.liquid`
+  );
+  console.log("Theme liquid", data);
+  if (!data.asset.value) {
+    return;
+  }
+
+  const snippet = fs.readFileSync(
+    path.resolve(__dirname, "../../liquid/theme.liquid")
+  );
+  let newPage = data.asset.value;
+  if (newPage.includes(snippet)) {
+    console.log("page already exists");
+    return;
+  }
+  // {% %}のインデントに注意
+  newPage = data.asset.value.replace(
+    "{% section 'header' %}",
+    `\n{% section 'header' %}\n${snippet}\n`
+  );
+  return newPage;
 }
 
 async function getThemeId(axios) {
